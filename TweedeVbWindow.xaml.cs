cffd12702b4e5c020dbcc953796a19226badad60
Crow Scare <ciki3100@gmail.com>
2020-05-06 17:31:55 +0200

    Add project files.

---
+++ TweedeVbWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HalloWereldWpfApp
+{
+    /// <summary>
+    /// Interaction logic for TweedeVbWindow.xaml
+    /// </summary>
+    public partial class TweedeVbWindow : Window
+    {
+        public TweedeVbWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Calendar1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (DateTime item in e.AddedItems) 
+            { GeselecteerdeDataListBox.Items.Add(item); }
+        }
+    }
+}
